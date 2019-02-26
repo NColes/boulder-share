b81397f2f8866f24efe1ac8460f959268597ef48
NColes <colesniall@gmail.com>
2019-02-26 23:06:55 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,59 @@
-$(document).ready(function() {
-  console.log("Script loaded");
+var WallContents = {};
+var WallRow = '<div class="wall-grid-row">';
+var WallCell = '<div class="wall-grid-cell"></div>';
 
-  $(".wall-grid-cell").click(function() {
-    // $(this).css("background-color", "green");
-  });
+var RowMarkup = '<div class="wall-grid-row">';
+var GridMarkup = "";
+
+$(document).ready(function() {
+
+  BuildWallGrid();
 
   $(".icon").click(function() {
     $(".selected").toggleClass("selected", false)
     $(this).toggleClass("selected");
   });
 
+  AttachClickEvent();
+
+});
+
+var BuildWallGrid = function() {
+  var RowCount = $(".wall-rows").val();
+  var ColumnCount = $(".wall-columns").val();
+
+  if (Object.keys(WallContents).length == 0) {
+    $(".wall-grid-cell").unbind();
+    $("#wall-grid").html("");
+
+    for (var i = 1; i <= ColumnCount; i++) {
+        RowMarkup += WallCell;
+        if (i == ColumnCount) {
+          RowMarkup += '</div>';
+          for (var c = 1; c <= RowCount; c++) {
+            GridMarkup += RowMarkup;
+          }
+        };
+    };
+    $("#wall-grid").html(GridMarkup);
+    AttachClickEvent();
+  };
+};
+
+var AttachClickEvent = function() {
   $(".wall-grid-cell").click(function() {
     if ($(".selected").length && !$(".selected").hasClass("icon-remove")) {
       console.log($(".selected").attr("src"));
       $(this).css("background-image", 'url(' + $(".selected").attr("src") + ')');
+      $(this).css("background-size", "contain");
     } else {
-      $(this).css("background-image", 'url("./images/baseline-add_circle-24px.svg")');
+      $(this).css("background-image", 'url("./images/baseline-add_circle-12px.svg")');
+      $(this).css("background-size", "auto");
     }
   });
-});
+};
+
+var ClearWall = function() {
+  $("#wall-grid").html(GridMarkup);
+  AttachClickEvent();
+}
